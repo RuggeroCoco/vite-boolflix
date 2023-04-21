8dd9359db9cae955e934bfe5a784674f18cf7425
RuggeroCoco <ruggerococo94@gmail.com>
2023-04-21 16:25:05 +0200

    Milestone 0+

--- src/store.js
+++ src/store.js
@@ -1,3 +1,8 @@
 import { reactive } from "vue";
 
-export const store = reactive({});
+export const store = reactive({
+  searchKey: "",
+  movies: [],
+  apiBaseUrl: "https://api.themoviedb.org/3",
+  loading: false,
+});
